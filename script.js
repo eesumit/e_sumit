de8c4d4f006e1c444a4ecca779b05750a9ccbb75
SUMIT <112788729+eesumit@users.noreply.github.com>
2023-10-24 18:20:50 +0530

    Update script.js
    update the repearting loop

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings:["e_sumit..."],
+    strings:["sumit welcomes you"],
     // strings:["sumit welcomes you","write your suggestion here","thanks for visiting"],
     typeSpeed:300,
     backSpeed:150,
